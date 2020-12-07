cd3db55017d2c9c90363d00a9ff85629eece5489
RyanC <ts-cuong.chau@rakuten.com>
2020-12-07 13:54:56 +0900

    Initial Commit

---
+++ TodoManagement/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoManagement
+//
+//  Created by Chau, Cuong | DCMS on 2020/12/07.
+//  Copyright © 2020 Chau, Cuong | DCMS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
